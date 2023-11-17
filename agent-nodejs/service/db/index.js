3ffa4ebdb7619530dc163f351b07348b044feaaa
Daniel Ribeiro <daniel.junior@gmail.com>
2023-11-17 11:15:35 -0300

    Ajuste do tamanho do buffer de leitura para 1MB

--- agent-nodejs/service/db/index.js
+++ agent-nodejs/service/db/index.js
@@ -9,28 +9,30 @@ class PersistenceTraceGc {
 
   createTable() {
     const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS dados (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        campo1 VARCHAR(255),
-        campo2 VARCHAR(255)
-        -- Adicione mais campos conforme necessário
-      );
+    CREATE TABLE tracegc(  
+      id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+      type VARCHAR(255),
+      timegc DECIMAL(4,2),
+      heap_used DECIMAL(10,2),
+      heap_cleaned DECIMAL(10,2),
+      gcfrequency INT
+    ) COMMENT '';
     `;
 
     this.connection.query(createTableQuery, (err) => {
-      if (err) throw err;
       console.log("Tabela criada ou já existe.");
     });
   }
 
   persistTrace(dataToPersist) {
-
-    if(dataToPersist.length < 5) {
-        // Encapsula o erro em uma nova exceção com mais informações
-        const novaExcecao = new Error(`Invalid dataToPersist content: ${dataToPersist}`);
-        novaExcecao.causaOriginal = err;
-        // Lança a nova exceção
-        throw novaExcecao;
+    if (dataToPersist.length < 5) {
+      // Encapsula o erro em uma nova exceção com mais informações
+      const novaExcecao = new Error(
+        `Invalid dataToPersist content: ${dataToPersist}`
+      );
+      novaExcecao.causaOriginal = err;
+      // Lança a nova exceção
+      throw novaExcecao;
     }
 
     const insertQuery = `
@@ -39,23 +41,21 @@ class PersistenceTraceGc {
     `;
 
     this.connection.query(insertQuery, dataToPersist, (err) => {
-
       if (err) {
         // Encapsula o erro em uma nova exceção com mais informações
-        const novaExcecao = new Error(`Insert Error. Variable dataToPersist: ${dataToPersist}`);
+        const novaExcecao = new Error(
+          `Insert Error. Variable dataToPersist: ${dataToPersist}`
+        );
         novaExcecao.causaOriginal = err;
         // Lança a nova exceção
         throw novaExcecao;
-      } 
-      
+      }
     });
-
   }
 
   closeDb() {
     this.connection.end();
   }
-  
 }
 
 const dbConfig = {
--- agent-nodejs/service/file/index.js
+++ agent-nodejs/service/file/index.js
@@ -1,20 +1,18 @@
 const fs = require("fs");
-const db = require("../db")
+const db = require("../db");
 
 class FileProcessor {
-
   readFileAndInsertData(filePath, fileStruct) {
-    
-    const stream = fs.createReadStream(filePath);
+    const stream = fs.createReadStream(filePath, {
+      highWaterMark: 1024 * 1024,
+    });
     const persistenceTrace = new db.PersistenceTraceGc();
 
     stream.on("data", (chunk) => {
       const lines = chunk.toString().split("\n");
 
       lines.forEach((line) => {
-
         if (line.trim().length > 0) {
-
           var dataLine = [];
 
           //1. Percorre cada coluna do fileStruct e busca na linha lida do arquivo a informacao
@@ -27,7 +25,9 @@ class FileProcessor {
             switch (columnData.tipo) {
               case "pattern": {
                 if (line.indexOf(columnData.pattern1) > -1) {
-                  var index1 = line.indexOf(columnData.pattern1) + columnData.pattern1.length;
+                  var index1 =
+                    line.indexOf(columnData.pattern1) +
+                    columnData.pattern1.length;
                   var index2 = line.indexOf(columnData.pattern2, index1);
                   data = line.substring(index1, index2).trim();
                 }
@@ -36,20 +36,18 @@ class FileProcessor {
 
             //Preenche dataLine usado no SQL
             if (data.length > 0) dataLine.push(data);
-
           });
 
           try {
             console.log(`Inserindo dados processados: ${dataLine}`);
             persistenceTrace.persistTrace(dataLine);
           } catch (err) {
-            console.log(`Ocorreu um erro com a persistencia da linha ${dataLine}. Continuando..`);
+            console.log(
+              `Ocorreu um erro com a persistencia da linha ${dataLine}. Continuando..`
+            );
           }
-
         }
-
       });
-
     });
 
     stream.on("error", (err) => {
