9e1aeda12149b3b4112bc3328f4ee6e8db079a70
Daniel Ribeiro <daniel.junior@gmail.com>
2023-11-23 10:20:20 -0300

    Correcao do processo terraform para container

--- api-rest-costumer/terraform/main.tf
+++ api-rest-costumer/terraform/main.tf
@@ -1,54 +1,26 @@
-provider "docker" {}
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
 
-resource "docker_network" "my_network" {
-  name = "my_network"
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-  name  = "nginx"
-  image = "nginx:latest"
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
   ports {
     internal = 80
-    external = 8080
+    external = 8000
   }
-  networks_advanced {
-    name = docker_network.my_network.name
-  }
-}
-
-resource "docker_image" "custom_app" {
-  name          = "custom_app"
-  build_context = "."
-  dockerfile    = "Dockerfile"
-}
-
-resource "docker_container" "app" {
-  name  = "app"
-  image = docker_image.custom_app.latest
-  ports {
-    internal = 5000
-    external = 5000
-  }
-  environment = ["DEBUG=True"]
-  depends_on  = [docker_container.nginx]
-  networks_advanced {
-    name = docker_network.my_network.name
-  }
-}
-
-resource "docker_container" "db" {
-  name  = "db"
-  image = "postgres:latest"
-  environment = {
-    POSTGRES_DB       = "mydatabase"
-    POSTGRES_USER     = "myuser"
-    POSTGRES_PASSWORD = "mypassword"
-  }
-  networks_advanced {
-    name = docker_network.my_network.name
-  }
-}
-
-output "app_container_ip" {
-  value = docker_container.app.networks_advanced[0].ipv4_address
 }
