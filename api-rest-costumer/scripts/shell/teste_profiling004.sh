411c0469ac923359cf54ebb1e59ca61d2f670ab1
Daniel Ribeiro <daniel.junior@gmail.com>
2023-11-24 07:15:02 -0300

    Inclusao cAdvisor e demais ajustes

---
+++ api-rest-costumer/scripts/shell/teste_profiling004.sh
@@ -0,0 +1,8 @@
+# TODO Na pasta raiz, onde se encontra Dockerfile
+$ docker build -e NOME_DO_PARAMETRO=valor_personalizado -t nome-da-imagem .
+
+
+#No windows da a mensagem 
+#What's Next?
+#  1. Sign in to your Docker account → docker login
+#  2. View a summary of image vulnerabilities and recommendations → docker scout quickview
