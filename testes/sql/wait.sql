91a49f6ad5262a1374811bfbcba0b2d9161dd687
Daniel Ribeiro <daniel.junior@gmail.com>
2023-11-26 19:58:35 -0300

    Updates

--- testes/sql/wait.sql
+++ testes/sql/wait.sql
@@ -1,11 +1,12 @@
+-- Active: 1699963435334@@127.0.0.1@3306@mydatabase
 DROP PROCEDURE IF EXISTS P_WAIT;
 
-CREATE PROCEDURE P_WAIT() BEGIN SELECT 
+CREATE PROCEDURE P_WAIT() 
+BEGIN  
 	SELECT SLEEP(16);
-	SELECT
-	    1 as idCostumer,
-	    'kyvqvdbxdmmwjadecvkzbbllupblfxtjuenoroxtuiucdeabgo' AS name;
-	END;
+	SELECT 1 as idCostumer,
+	    'Daniel Ribeiro' AS name;
+END;
 
 
 call `P_WAIT`
