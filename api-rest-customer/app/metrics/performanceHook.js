2fd850a61a6480d58fce9a79e1e1cbfceea5295e
Daniel Ribeiro <daniel.junior@gmail.com>
2023-11-30 16:05:34 -0300

    Diversos ajustes

---
+++ api-rest-customer/app/metrics/performanceHook.js
@@ -0,0 +1,65 @@
+const { builtinModules } = require("module");
+const { PerformanceObserver } = require("perf_hooks");
+const { InitGc25Counter, IncGc25Counter } = require("./manageGcCounter");
+
+let gcStatistics = {
+  type: "",
+  diffGCTime: 0,
+  gcPercentil: 0,
+  lastStartTime: 0,
+  gcDuration: 0,
+};
+
+// Create a performance observer
+const obs = new PerformanceObserver((list) => {
+  gc_event_callback(list);
+});
+
+function gc_event_callback(list) {
+  const entry = list.getEntries()[0];
+  updateStatistics(entry);
+  console.log(`########## Evento GC ${JSON.stringify(gcStatistics)}`);
+
+}
+
+
+function updateStatistics(entry) {
+  if (entry.entryType == "gc") {
+
+    gcStatistics.type = "gc";
+
+    //Variavel de calculo
+    let atualGCStartTime = entry.startTime;
+    let gcPercentil = 0;
+
+    //Calculos
+    gcStatistics.gcDuration = entry.duration;
+    gcStatistics.lastStartTime = entry.lastStartTime;
+    gcStatistics.diffGCTime = gcStatistics.lastStartTime > 0 ? atualGCStartTime - gcStatistics.lastStartTime : 0;
+
+    if (gcStatistics.diffGCTime > 0) {
+      gcPercentil = (gcStatistics.gcDuration / gcStatistics.diffGCTime) * 100;
+      gcStatistics.gcPercentil = gcPercentil;
+    }
+
+    //Variavel de calculo
+    gcStatistics.lastStartTime = entry.startTime;
+
+    //TODO Atualiza metrica prometheus
+
+  }
+}
+
+function observe() {
+  // Subscribe to notifications of GCs
+  obs.observe({ entryTypes: ["gc"] });
+  // Registrar nova metrica de estatistica
+  
+}
+
+function disconnect() {
+  // Stop subscription
+  obs.disconnect();
+}
+
+module.exports = { observe, disconnect };
---
+++ cenarios/analises/analise_tipos_imagem.md
@@ -0,0 +1,7 @@
+Comparar:
+
+objter a menos imagem possivel para rodar a api_rest_customer
+
+a. Tamanho da Imagem x consumo de memoria 
+b. Tamanho da Imagem x throughput
+
