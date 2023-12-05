2ba49ad22052f6019d66a991a35b0f2c29aa8193
Daniel Ribeiro <daniel.junior@gmail.com>
2023-12-05 12:22:18 -0300

    Ajustes

---
+++ estudo-rest-node-java/java_rest_api/run-image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 8090:8090 springboot.rest.api 
---
+++ estudo-rest-node-java/node_rest_api/index-axios-java.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function fetchData() {
+  try {
+    const response = await axios.get('https://api-rest-java/api/async');
+    console.log(response.data);
+  } catch (error) {
+    console.error('Erro ao obter dados:', error.message);
+  }
+}
+
+fetchData();
+
+setInterval(() => {console.log("I'm alive!")}, 1000);
