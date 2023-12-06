83d36a51d27ce684f34b8808e0b34b91d806e248
Daniel Ribeiro <daniel.junior@gmail.com>
2023-12-06 16:13:23 -0300

    Update

--- estudo-rest-node-java/java_rest_api/src/main/java/com/example/study/RestApiConsumer.java
+++ estudo-rest-node-java/java_rest_api/src/main/java/com/example/study/RestApiConsumer.java
@@ -1,16 +1,46 @@
 package com.example.study;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 public class RestApiConsumer {
 
 
     private RestTemplate restTemplate;
+    private static String apiUrl = "https://api.example.com/data";
+
+    public static void main(String[] args) {
+        
+        fetchDataAsync(apiUrl);
+    }
+
+    private static void fetchDataAsync(String apiUrl) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .build();
+
+        CompletableFuture<Void> future = httpClient
+                .sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(responseBody -> {
+                    System.out.println("Response: " + responseBody);
+                })
+                .exceptionally(throwable -> {
+                    System.err.println("Erro ao consumir a API: " + throwable.getMessage());
+                    return null;
+                });
+
+        // Espera a conclusão da requisição assíncrona
+        future.join();
+    }
 
     public void ExternalApiController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
